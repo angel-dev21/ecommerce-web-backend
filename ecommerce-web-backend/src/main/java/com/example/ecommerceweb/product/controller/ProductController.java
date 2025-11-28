8c5120bf3791c44044888cba408c3cc82ed22950
angel-dev21 <146152337+angel-dev21@users.noreply.github.com>
2025-11-27 19:47:56 -0500

    Backend: Added pagination and sorting in product endpoint.

--- ecommerce-web-backend/src/main/java/com/example/ecommerceweb/product/controller/ProductController.java
+++ ecommerce-web-backend/src/main/java/com/example/ecommerceweb/product/controller/ProductController.java
@@ -1,11 +1,14 @@
 package com.example.ecommerceweb.product.controller;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.ecommerceweb.product.dto.ProductCatalogDto;
@@ -22,9 +25,15 @@ public class ProductController {
 		this.productService = productService;
 	}
 
-	@GetMapping("/catalog")
-	public ResponseEntity<List<ProductCatalogDto>> getAllProductCatalog() {
-		return new ResponseEntity<>(productService.getAllProductCatalog(), HttpStatus.OK);
+	@GetMapping
+	public ResponseEntity<Page<ProductCatalogDto>> getAllProducts(
+			@RequestParam(defaultValue = "0") int page,
+			@RequestParam(defaultValue = "20") int size,
+			@RequestParam(defaultValue = "id") String sortBy,
+			@RequestParam(defaultValue = "true") boolean order) {
+		Sort sort = order ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+		Pageable pageable = PageRequest.of(page, size, sort);
+		return new ResponseEntity<>(productService.getAllProducts(pageable), HttpStatus.OK);
 	}
 
 }
--- ecommerce-web-backend/src/main/java/com/example/ecommerceweb/product/service/ProductService.java
+++ ecommerce-web-backend/src/main/java/com/example/ecommerceweb/product/service/ProductService.java
@@ -1,8 +1,7 @@
 package com.example.ecommerceweb.product.service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,9 +20,9 @@ public class ProductService {
 	}
 	
 	@Transactional(readOnly = true)
-	public List<ProductCatalogDto> getAllProductCatalog(){
-		List<ProductEntity> productEntity = productRepository.findAll();
-		List<ProductCatalogDto> productCatalogDto = productEntity.stream().map(product -> ProductMapper.productEntityToCatalogDto(product)).collect(Collectors.toList());
+	public Page<ProductCatalogDto> getAllProducts(Pageable pageable){
+		Page<ProductEntity> productEntity = productRepository.findAll(pageable);
+		Page<ProductCatalogDto> productCatalogDto = productEntity.map(product -> ProductMapper.productEntityToCatalogDto(product));
 		return productCatalogDto;
 	}
 }
